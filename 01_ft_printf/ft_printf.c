be062ce9a58912b31c99394d4ae396d3c7aef437
cheun0327 <cheun0327@gmail.com>
2021-06-20 00:59:01 +0900

    string

--- 01_ft_printf/ft_printf.c
+++ 01_ft_printf/ft_printf.c
@@ -6,26 +6,26 @@
 /*   By: chaeekim <chaeekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 13:23:41 by chaeekim          #+#    #+#             */
-/*   Updated: 2021/06/14 12:26:10 by chaeekim         ###   ########.fr       */
+/*   Updated: 2021/06/19 23:55:07 by chaeekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int			ft_putchar_fd(char c, int fd)
+int			ft_putchar(char c)
 {
-	return (write(fd, &c, 1));
+	return (write(1, &c, 1));
 }
 
-int			ft_putstr_fd(char *s, int fd)
+int			ft_putstr(char *s)
 {
 	size_t	i;
 
 	i = 0;
 	while (s[i])
 	{
-		write(fd, &s[i], 1);
+		write(1, &s[i], 1);
 		i++;
 	}
 	return (i);
@@ -45,6 +45,42 @@ char		*ft_strchr(const char *s, int c)
 	if (c == 0)
 		return ((char *)&s[i]);
 	return (0);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2, int free_num)
+{
+	char	*ptr;
+	int		len;
+	int		i;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	else if (!(s1) || !(s2))
+		return (!(s1) ? ft_strdup(s2) : ft_strdup(s1));
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (*s1 != 0)
+		ptr[i++] = *s1++;
+	while (*s2 != 0)
+		ptr[i++] = *s2++;
+	ptr[i] = 0;
+	if (free_num == 1 || free_num == 3)
+		free (s1);
+	if (free_num == 2 || free_num == 3)
+		free (s2);
+	return (ptr);
 }
 
 int			ft_isdigit(int c)
@@ -97,20 +133,112 @@ void		check_width_and_prec(va_list ap, char *format, t_info *info, int i)
 		else
 			info->prec = va_arg(ap, int);
 	}
-	print_t_info(info);
 }
 
-int			print_char(int arg, t_info *info)
+/*
+** Character, %
+*/
+
+int			put_char_width(int width, int len, int zero)
 {
-	printf("--%d\n", arg);
-	print_t_info(info);
-	return (arg);
+	int		ret;
+
+	ret = 0;
+	while (len < width)
+	{
+		//char은 해당 없지만, %는 '0'flag 가능
+		if (zero == 1)
+			ft_putchar('0');
+		else
+			ft_putchar(' ');
+		len++;
+		ret++;
+	}
+	return (ret);
 }
 
-int			print_str(char * arg, t_info * info)
+int			print_char(int c, t_info *info)
 {
-	printf("--%s\n", arg);
-	print_t_info(info);
+	int		ret;
+
+	ret = 0;
+	//%타입 이거나 '-'왼쪽 정렬 있으면 zero padding 무시
+	if (info->type == '%' && info->minus == 1)
+		info->zero = 0;
+	//왼쪽 정렬 있으면 문자를 먼저 출력해줌
+	if (info->minus == 1)
+		ret += ft_putchar(c);
+	//width계산해서 출력
+	ret += put_char_width(info->width, 1, info->zero);
+	//왼쪽 정렬 아니었으면 width다음으로 문자 출력
+	if (info->minus == 0)
+		ret += ft_putchar(c);
+	return (ret);
+}
+
+/*
+** String
+*/
+
+char			*set_buf(char *str, int prec, int len)
+{
+	int			i;
+	int			end;
+	char		*buf;
+	
+	end = prec < len ? prec : len;
+	if (!(buf = (char *)malloc(sizeof(char) * end + 1)))
+		return (NULL);
+	i = 0;
+	while (i < end)
+	{
+		buf[i] = str[i];
+		i++;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
+
+int			put_str_width(char **buf, t_info *info)
+{
+	char	**width;
+	int		i;
+	
+	//TODO int로 형변환 안하면 어떻게 됨??
+	if (info->width <= (int)ft_strlen(*buf))
+		return ((int)ft_strlen(*buf));
+	width = (char *)malloc(sizeof(char) * (info->width - ft_strlen(*buf) + 1));
+	i = 0;
+	while ((size_t)i < info->width - ft_strlen(*buf))
+	{
+		width[i] = (info->zero == 1) ? '0' : ' ';
+		i++;
+	}
+	width[i] = '\0';
+	if (info->minus == 0)
+		*buf = ft_strjoin(width, *buf, 3);
+	else
+		*buf = ft_strjoin(*buf, width, 3);
+	//TODO return ((int)ft_strlen(*buf));
+	return (info->width);
+	
+}
+
+int			print_str(char * str, t_info * info)
+{
+	int		ret;
+	char	*buf;
+	
+	ret = 0;
+	// TODO NULL인 경우 어떻게 처리하는지 확인
+	if (str == NULL)
+		str = "(null)";
+	if (info->prec == -1 || (size_t)info->prec > ft_strlen(str))
+		info->prec = ft_strlen(str);
+	buf = set_buf(str, info->prec, ft_strlen(str));
+	ret = put_str_width(&buf, info);
+	ft_putstr(buf);
+	free(buf);
 	return (0);
 }
 
@@ -128,10 +256,12 @@ int			print_type(va_list ap, t_info *info)
 
 	ret = 0;
 	type = info->type;
+	print_t_info(info);
 	if (type == 'c')
 		ret = print_char(va_arg(ap, int), info);	//ascii값으로 읽는다 -> int
 	else if (type == '%')
 		ret = print_char('%', info);
+	
 	else if (type == 's')
 		ret = print_str(va_arg(ap, char *), info);
 	else if (type == 'd' || type == 'i')
@@ -153,7 +283,6 @@ void		check_info(va_list ap, char *format, t_info *info, int i)
 		info->prec = 0;					
 	else if (ft_isdigit(format[i]) || format[i] == '*')
 		check_width_and_prec(ap, format, info, i);
-	print_t_info(info);
 }
 
 int			parse_format(va_list ap, char *format)
@@ -169,7 +298,7 @@ int			parse_format(va_list ap, char *format)
 	while (format[i] != '\0')
 	{
 		while (format[i] != '%' && format[i] != '\0')
-			ret += ft_putchar_fd(format[i++], 1);
+			ret += ft_putchar(format[i++]);
 		if (format[i] == '%')
 		{
 			init_info(info);
@@ -199,13 +328,21 @@ int			ft_printf(const char *format, ...)
 int			main()
 {
 	char *s = "Hello";
-	char c = 'a';
+	// char c = 'a';
 	
-	ft_printf("[%-03.*c]", 5, c);
+	// CHAR DONE
+	// ft_printf("[%-5.*c]", 5, c);
+	// ft_printf("[%-5.*c]", 5, c);
+	// ft_printf("[%05%]");
+	// ft_printf("[%-05%]");
+	// ft_printf("[%-*%]", 5);
+
+	// STRING ING~
 	ft_printf("%s", s);
-	ft_printf("%d", 10);
-	ft_printf("%u", 1234);
-	ft_printf("%x")
+	
+	// ft_printf("%d", 10);
+	// ft_printf("%u", 1234);
+	// ft_printf("%x")
 	
 	return 0;
 }
